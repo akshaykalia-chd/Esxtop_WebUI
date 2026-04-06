407371dedd34ed7590d3e56b148bac4b65c17252
Akshay Kalia <61831701+akshaykalia-chd@users.noreply.github.com>
2026-04-06 21:11:57 +0530

    Moves Systen Objects to a configuration

--- const.py
+++ const.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 home = Path.home()
 LOG_FILE = f'{home}/esxtop_drill.log'
+
+# Default object name fragments used to filter out ESXi system/background objects.
+SYSTEM_OBJECT_PATTERNS = [
+    ':system', ':helper', ':drivers', ':ft', ':vmotion', ':init', ':vmsyslogd', ':sh', ':vobd', ':vmkeventd',
+    ':vmkdevmgr', ':net-lacp', ':dhclient-uw', ':vmkiscsid', ':nfsgssd', ':busybox', ':ntpd',
+    ':vmware-usbarbitrator', ':ioFilterVPServer', ':swapobjd', ':storageRM', ':hostdCgiServer', ':sensord',
+    ':net-lbt', ':hostd', ':rhttpproxy', ':slpd', ':net-cdp', ':nscd', ':smartd', ':lwsmd', ':pktcap-agent',
+    ':netcpa', ':vdpi', ':logchannellogger', ':logger', ':dcui', ':vpxa', ':fdm', ':vsfwd', ':sfcbd',
+    ':sfcb-sfcb', ':sfcb-ProviderMa', ':openwsmand', ':sshd', ':esxtop', ':gzip', ':sdrsInjector', ':timeout'
+]
