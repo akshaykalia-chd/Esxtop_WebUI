5d503d73fd606db70fbd46f44c0a750e93b3ef61
Akshay Kalia <61831701+akshaykalia-chd@users.noreply.github.com>
2026-04-07 20:54:12 +0530

    update to ignored files

--- filters_ops.py
+++ filters_ops.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import time
 
 import numpy as np
@@ -32,26 +33,23 @@ def filter_objects(working_dir, esxtop_data_frame):
             val_err = True
         else:
             val_err = False
-    cdf = pd.DataFrame(esxtop_data_frame.columns)
-    cdf.columns = ['Objects']
     # Processing data based on user input1
     out_df = esxtop_data_frame
     if len(object_selection) != 0:
-        column_list = []
-        for Object in object_selection:
-            column_list.extend(cdf.index[cdf['Objects'].str.contains(Object)].tolist())
-            column_list.sort(reverse=False)
-        col_name = []
-        col_name.insert(0, cdf['Objects'][0])
-        i = 1
-        for col in column_list:
-            col_name.insert(i, cdf['Objects'][col])
-            i = i + 1
-            out_df = esxtop_data_frame[col_name]
+        cols = esxtop_data_frame.columns.astype(str)
+        escaped_items = [re.escape(obj) for obj in object_selection]
+        combined_pattern = "|".join(escaped_items)
+        matched_idx = cols.str.contains(combined_pattern, regex=True, na=False)
+        matched_positions = np.where(matched_idx.to_numpy())[0].tolist()
+        matched_positions.sort()
+        selected_positions = [0] + matched_positions
+        # Keep order stable but avoid duplicate timestamp column selection.
+        selected_positions = list(dict.fromkeys(selected_positions))
+        col_name = cols.take(selected_positions).tolist()
+        out_df = esxtop_data_frame[col_name]
 
-        col_name_df = pd.DataFrame(out_df.columns)
         # Validating data in Object filtered output
-        if col_name_df[0].count() <= 1:
+        if len(out_df.columns) <= 1:
             error("Unable to find the objects specified.Program will exit")
             return
         outfile = str(object_selection[0] + str(int(time.time())) + ".csv")
@@ -64,18 +62,12 @@ def filter_objects(working_dir, esxtop_data_frame):
 # --------------------------------------------------------------------------------------
 # Function to implement Counter Group filtration
 def filer_counter_group(object_filtered_data_frame, cg_selection, working_dir):
-    col_name_df = pd.DataFrame(object_filtered_data_frame.columns)
-    c_gn_c_df = col_name_df[0].str.split("\\", expand=True)
-    cg_df = c_gn_c_df[3].str.split("\\(", expand=True)
-    c_gn_c_df[3] = cg_df[0]
-    column_list = [0]
-    column_list.extend(c_gn_c_df.index[c_gn_c_df[3] == cg_selection].tolist())
-    column_list.sort(reverse=False)
-    col_name = []
-    i = 0
-    for col in column_list:
-        col_name.insert(i, col_name_df[0][col])
-        i = i + 1
+    cols = object_filtered_data_frame.columns.astype(str)
+    split_cols = cols.str.split("\\", expand=True)
+    counter_groups = split_cols[3].str.split("\\(", expand=True)[0]
+    column_list = [0] + np.where(counter_groups.eq(cg_selection).to_numpy())[0].tolist()
+    column_list.sort()
+    col_name = cols.take(column_list).tolist()
     out_df = object_filtered_data_frame[col_name]
     outfile = str(cg_selection + "-" + str(int(time.time())) + ".csv")
     outfile = os.path.join(working_dir, cg_selection, outfile)
@@ -88,20 +80,15 @@ def filer_counter_group(object_filtered_data_frame, cg_selection, working_dir):
 # Function to implement Counter filtration
 def filer_counter(cg_filtered_data_frame, c_selection, cg_selection, working_dir):
     out_df = cg_filtered_data_frame
-    col_name_df = pd.DataFrame(cg_filtered_data_frame.columns)
-    time_se = str(col_name_df[0][0])
-    col_name_df = col_name_df.drop([0])
-    c_gn_c_df = col_name_df[0].str.split("\\", expand=True)
-    column_list = list()
+    cols = cg_filtered_data_frame.columns.astype(str)
+    time_se = str(cols[0])
+    data_cols = cols[1:]
+    split_cols = data_cols.str.split("\\", expand=True)
     try:
-        column_list.extend(c_gn_c_df.index[c_gn_c_df[4] == c_selection].tolist())
-        column_list.sort(reverse=False)
-        col_name = list()
-        col_name.append(time_se)
-        i = 1
-        for Col in column_list:
-            col_name.insert(i, col_name_df[0][Col])
-            i = i + 1
+        column_list = np.where(split_cols[4].eq(c_selection).to_numpy())[0].tolist()
+        column_list.sort()
+        col_name = [time_se]
+        col_name.extend(data_cols.take(column_list).tolist())
         out_df = out_df[col_name]
         outfile = str(cg_selection + "-" + c_selection + "-" + str(int(time.time())) + ".csv")
         outfile = os.path.join(working_dir, cg_selection, outfile)
@@ -123,14 +110,10 @@ def filer_counter(cg_filtered_data_frame, c_selection, cg_selection, working_dir
 # ------------------------------------------------------------------------------------
 # Function to prepare Counter group selection list
 def prep_cg_selection(object_filtered_data_frame):
-    col_name_df = pd.DataFrame(object_filtered_data_frame.columns)
-    c_gn_c_df = col_name_df[0].str.split("\\", expand=True)
-    cg_df = c_gn_c_df[3].str.split("\\(", expand=True)
-    c_gn_c_df[3] = cg_df[0]
-    counter_groups = pd.DataFrame(cg_df[0].unique())
-    counter_groups.columns = ['Counter Groups']
-    counter_groups = counter_groups.replace(to_replace='None', value=np.nan).dropna()
-    selection_list = counter_groups['Counter Groups'].tolist()
+    cols = object_filtered_data_frame.columns.astype(str)
+    split_cols = cols.str.split("\\", expand=True)
+    counter_groups = split_cols[3].str.split("\\(", expand=True)[0]
+    selection_list = counter_groups.replace('None', np.nan).dropna().unique().tolist()
     return selection_list
 
 
@@ -139,14 +122,10 @@ def prep_cg_selection(object_filtered_data_frame):
 def drop_sys_obj(data):
     sel = question('Do you want to drop system objects like vpxa workers, hostd workers etc?')
     if sel == 2:
-        c_df = pd.DataFrame(data.columns)
-        c_index_list = list()
-        for obj in SYSTEM_OBJECT_PATTERNS:
-            c_index_list.extend(c_df.index[c_df[0].str.contains(obj)].tolist())
-        c_name_list = list()
-        for c_index in c_index_list:
-            c_name = c_df.at[c_index, 0]
-            c_name_list.append(c_name)
+        cols = data.columns.astype(str)
+        escaped_patterns = [re.escape(obj) for obj in SYSTEM_OBJECT_PATTERNS]
+        combined_pattern = "|".join(escaped_patterns)
+        c_name_list = cols[cols.str.contains(combined_pattern, regex=True, na=False)].tolist()
         out_df = data.drop(c_name_list, axis=1)
         logging.info('System objects dropped')
         return out_df
